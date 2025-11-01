aa7d9202ab62425e61f146a238c7b8f7f72da683
A Student <student@nowhere.edu>
2025-11-01 23:42:40 +0000

    Nov 1, 2025, 4:42 PM

--- main.cpp
+++ main.cpp
@@ -23,4 +23,4 @@ while (balance > 0) {
   if (balance < 0)
      balance = 0;
 
-  cout << month << "\t" << interest << "\t" << payment << "\t" << balance << endl;
+  cout << month << "\t" << interest << "\t" << payment << "\t" << balance << endl; }
